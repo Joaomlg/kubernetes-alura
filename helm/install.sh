696e6932a44860c6cf4c3d31f758babb3f52a4cc
joaomlg <joaomarcos1999jm@gmail.com>
2023-03-11 19:38:03 -0300

    first commit

---
+++ helm/install.sh
@@ -0,0 +1,3 @@
+helm upgrade --install -f values/db-noticias.yaml db-noticias .
+helm upgrade --install -f values/sistema-noticias.yaml sistema-noticias .
+helm upgrade --install -f values/portal-noticias.yaml portal-noticias .
